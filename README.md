ab408513b3ed434a5b56f9e13715af7bc184ae87
Joserra2304 <joserra2304@gmail.com>
2024-05-16 23:04:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# glutenvoid_app
+
+Desarrollo de una aplicación movil para usuarios con trastornos alimenticios provocados por el gluten
+
---
+++ android/app/src/main/kotlin/com/svalero/glutenvoid_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.svalero.glutenvoid_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
